a8edaf670747fefc373893d46777ad07149540af
SiapaLupa <muhammadilyasafadhlih@gmail.com>
2023-06-01 16:52:28 +0700

    cut some env variable

--- config/db.js
+++ config/db.js
@@ -2,13 +2,11 @@
 
 const mongoose = require('mongoose')
 
-const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_PORT = process.env.DB_PORT || 27017
-const DB_NAME = process.env.DB_NAME
+const uri = process.env.MONGODB_CONNECTION_STRING
 
 mongoose.set('strictQuery', true)
 mongoose.connect(
-    `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+    uri,
     () => console.info('Connected to DB')
 )
 
